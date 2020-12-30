7fd13507004604d33803a420e5c53b661a5da2f1
Jiří Hejdušek <hejdusek.j@gmail.com>
2020-12-30 16:31:04 +0100

    pridani typu

--- src/data_analysis/models/views.py
+++ src/data_analysis/models/views.py
@@ -1,22 +1,57 @@
 from ...globals.db import DB
-from sqlalchemy import Table, Column, Integer
-
+from sqlalchemy import Table, Column, Integer, String, DATETIME, DECIMAL, Boolean
+from typing import List
 
 
 db = DB.get_globals()
 
 
-class ViewWithoutRes(db.DECLARATIVE_BASE):
+class ViewTypeWithoutRes:
+    id = None
+    symbol = None
+    open_time = None
+    open_price = None
+    high_price = None
+    low_price = None
+    close_price = None
+    volume = None
+    close_time = None
+    quote_asset_volume = None
+    number_of_trades = None
+    taker_buy_base_asset_volume = None
+    taker_buy_quote_asset_volume = None
+    sma21 = None
+    sma200 = None
+    ema21 = None
+    ema200 = None
+
+
+class ViewWithoutRes(ViewTypeWithoutRes, db.DECLARATIVE_BASE):
     __table__ = Table("VJoined", db.DECLARATIVE_BASE.metadata,
                       Column("id", Integer, primary_key=True),
                              autoload=True, autoload_with=db.ENGINE
                     )
 
 
-class ViewWithtRes(DB.DECLARATIVE_BASE):
+class ViewTypeWithRes(ViewTypeWithoutRes):
+    up = None
+    down = None
+
+
+class ViewWithtRes(ViewTypeWithRes, DB.DECLARATIVE_BASE):
     __table__ = Table("VJoinedVRes", DB.DECLARATIVE_BASE.metadata,
-                      Column("id", Integer, primary_key=True),
-                      autoload=True, autoload_with=db.ENGINE,
+                        Column("id", Integer, primary_key=True),
+                        autoload=True, autoload_with=db.ENGINE,
                     )
 
+    def __repr__(self):
+        return f"symbol={self.symbol} open_time={self.open_time} up={self.up} down={self.down}"
 
+
+"""
+Priklad pouziti.. dulezite je pridat typ List[ViewTypeWithRes] potom to bude radit typy
+def test(*args):
+    res: List[ViewTypeWithRes] = db.SESSION.query(ViewWithtRes)
+    for r in res:
+        print(r.symbol)
+"""
