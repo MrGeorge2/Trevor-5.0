9d06a5ddaf47adec72a55d1b5e91b204f0a8d07f
Jiří Hejdušek <hejdusek.j@gmail.com>
2021-01-30 11:20:56 +0100

    zmena navrhoveho modelu

--- src/live_trading/order.py
+++ src/live_trading/order.py
@@ -5,8 +5,30 @@ from typing import Optional
 from decimal import Decimal
 
 
-class Order:
+class OrderInterface:
+    def set_order_id(self):
+        pass
 
+    def open(self):
+        pass
+
+    def close(self):
+        pass
+
+    @property
+    def is_opened(self) -> bool:
+        return False
+
+    @property
+    def is_filled(self) -> bool:
+        return False
+
+    @property
+    def is_closed(self) -> bool:
+        return False
+
+
+class Order(OrderInterface):
     def __init__(self, open_price: Decimal, symbol: str, order_type: str):
         self.open_price: Decimal = open_price
         self.symbol: str = symbol
@@ -14,8 +36,9 @@ class Order:
         self.order_type: str = order_type
         self._order_id: Optional[str] = None
 
+        self.filled: Optional[bool] = False
+        self.opened: Optional[bool] = False
         self.closed: Optional[bool] = False
-        self.close_price: Optional[Decimal] = None
         self.close_time: Optional[datetime] = None
 
     def set_order_id(self, order_id):
@@ -24,24 +47,22 @@ class Order:
     def open(self):
         pass
 
-    def close(self, close_price):
-        self.close_price = close_price
-        self.close_time = datetime.now()
-        self.closed = True
+    def close(self):
+        if self.is_opened:
+            self.close_time = datetime.now()
+            self.closed = True
+
+    @property
+    def is_opened(self):
+        return self.opened
 
     @property
     def is_filled(self) -> bool:
-        return True
+        return self.filled
 
     @property
     def is_closed(self) -> bool:
         return self.closed
-
-    def get_profit(self) -> float:
-        if self.is_closed:
-            return (Decimal(self.close_price - self.open_price) / self.open_price) * 100
-        else:
-            Decimal(0)
 
 
 class InitOrder(Order):
@@ -56,18 +77,41 @@ class TakeProfitOrder(Order):
     pass
 
 
-class Long:
+class FullOrderBase(OrderInterface):
     def __init__(self, init_order: InitOrder, stop_loss: StopLossOrder, take_profit: TakeProfitOrder):
         self.init_order = init_order
         self.stop_loss = stop_loss
         self.take_profit = take_profit
 
+    def get_profit(self) -> float:
+        if self.stop_loss.is_filled:
+            return (Decimal(self.stop_loss.open_price - self.init_order.open_price) / self.init_order.open_price) * 100
+        elif self.take_profit.is_filled:
+            return (Decimal(self.stop_loss.open_price - self.init_order.open_price) / self.init_order.open_price) * 100
+        else:
+            Decimal(0)
 
-class Short:
-    def __init__(self, init_order: InitOrder, stop_loss: StopLossOrder, take_profit: TakeProfitOrder):
-        self.init_order = init_order
-        self.stop_loss = stop_loss
-        self.take_profit = take_profit
+    def close(self):
+        self.init_order.close()
+        self.take_profit.close()
+        self.stop_loss.close()
+
+    def open_stop_loss(self):
+        self.take_profit.close()
+        self.stop_loss.open()
+
+    def open_take_profit(self):
+        self.stop_loss.close()
+        self.take_profit.open()
+
+
+class Long(FullOrderBase):
+    pass
+
+
+class Short(FullOrderBase):
+    pass
+
 
 #TODO: ziskat svicky z api
     #TODO: preprocessing
