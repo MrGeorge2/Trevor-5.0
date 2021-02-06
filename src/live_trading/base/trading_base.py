a02b1861df43b5cdfcba3e9580af22c1681e82a0
luke-20 <46968909+luke-20@users.noreply.github.com>
2021-02-06 19:35:16 +0100

    Update trading_base.py

--- src/live_trading/base/trading_base.py
+++ src/live_trading/base/trading_base.py
@@ -95,14 +95,12 @@ class TradingInterface:
         if prediction == 1 and not self.manager.is_order_already_opened(last_candle=last_candle, prediction=prediction):
             tp: Decimal = last_candle.close_price * Decimal((1 + 0.33 / 100))
             sl: Decimal = last_candle.close_price * Decimal((1 - 0.1 / 100))
-
             self.manager.open_long(price=last_candle.close_price, take_profit=tp, stop_loss=sl, last_candle=last_candle)
             return True
 
         elif prediction == 0 and not self.manager.is_order_already_opened(last_candle=last_candle, prediction=prediction):
             tp: Decimal = last_candle.close_price * Decimal((1 - 0.3 / 100))
             sl: Decimal = last_candle.close_price * Decimal((1 + 0.1 / 100))
-
             self.manager.open_short(price=last_candle.close_price, take_profit=tp, stop_loss=sl, last_candle=last_candle)
             return True
 
