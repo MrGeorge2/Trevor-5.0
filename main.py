eb66e4aeb1730b9b1dd5f2b5885cb7252b099fb5
Jiří Hejdušek <hejdusek.j@gmail.com>
2021-02-06 18:26:59 +0100

    oprava scriptu

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import sys
 # První parametr pro metodu
 DEBUG_SYS_ARGV: List[str] = [
     " ",                
-    "backtest",
+    "full_fetch",
 ]
 
 DEBUG: bool = True
--- src/live_trading/base/trading_base.py
+++ src/live_trading/base/trading_base.py
@@ -87,18 +87,14 @@ class TradingInterface:
     def _create_order(self, prediction, last_candle: CandleApi):
 
         if prediction == 1 and not self.manager.is_order_already_opened(last_candle=last_candle, prediction=prediction):
-            # tp: Decimal = last_candle.close_price * Decimal((1 + 0.212 / 100))
-            tp: Decimal = last_candle.close_price * Decimal((1 + 0.35 / 100))
-            # sl: Decimal = last_candle.close_price * Decimal((1 - 0.05 / 100))
-            sl: Decimal = last_candle.close_price * Decimal((1 - 0.5 / 100))
+            tp: Decimal = last_candle.close_price * Decimal((1 + 0.33 / 100))
+            sl: Decimal = last_candle.close_price * Decimal((1 - 0.1 / 100))
             self.manager.open_long(price=last_candle.close_price, take_profit=tp, stop_loss=sl, last_candle=last_candle)
             return True
 
         elif prediction == 0 and not self.manager.is_order_already_opened(last_candle=last_candle, prediction=prediction):
-            # tp: Decimal = last_candle.close_price * Decimal((1 - 0.25 / 100))
-            tp: Decimal = last_candle.close_price * Decimal((1 - 0.35 / 100))
-            # sl: Decimal = last_candle.close_price * Decimal((1 + 0.05 / 100))
-            sl: Decimal = last_candle.close_price * Decimal((1 + 0.5 / 100))
+            tp: Decimal = last_candle.close_price * Decimal((1 - 0.3 / 100))
+            sl: Decimal = last_candle.close_price * Decimal((1 + 0.1 / 100))
             self.manager.open_short(price=last_candle.close_price, take_profit=tp, stop_loss=sl, last_candle=last_candle)
             return True
 
