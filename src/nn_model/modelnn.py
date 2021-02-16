febc6b8817d8cb1548d39ddcd355eaa4f32f48a7
luke-20 <46968909+luke-20@users.noreply.github.com>
2021-02-16 11:47:05 +0100

    ok

--- src/nn_model/modelnn.py
+++ src/nn_model/modelnn.py
@@ -61,12 +61,6 @@ class ModelNN:
         model.add(Dropout(0.2))
         model.add(BatchNormalization())
 
-        model.add(Dense(128, activation='relu'))
-        model.add(Dropout(0.2))
-
-        model.add(Dense(64, activation='relu'))
-        model.add(Dropout(0.2))
-
         model.add(Dense(32, activation='relu'))
         model.add(Dropout(0.2))
 
